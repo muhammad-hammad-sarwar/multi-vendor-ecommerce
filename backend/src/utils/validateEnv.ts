550088f9c35b966444ca805eb584904aadd9b4b1
muhammad-hammad-sarwar <hammadsarwar2200@gmail.com>
2026-07-11 19:07:35 +0500

    feat: remove console log for environment variable validation

--- backend/src/utils/validateEnv.ts
+++ backend/src/utils/validateEnv.ts
@@ -2,7 +2,6 @@ export default function validateEnv() {
   const required = ["JWT_SECRET", "DB_URL", "APP_EMAIL", "APP_PASSWORD"];
 
   for (const key of required) {
-    console.log("Validate ENV ", process.env[key]);
     if (!process.env[key]) {
       throw new Error(`Missing environment variable: ${key}`);
     }
